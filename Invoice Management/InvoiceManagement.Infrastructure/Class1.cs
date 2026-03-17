9cc50a361cd9f07e27d0098837c96201afdb98f5
Monde Deyi <mundaright39@example.com>
2026-03-17 22:19:56 +0200

    - Added ApplicationDbContext for EF6 persistence - Created generic repository interface and implementation - Implemented Unit of Work pattern for transaction management - Structured Infrastructure layer (Persistence, Repository, UnitOfWork) - Configured async query methods using EF6 - Removed placeholder classes and cleaned project structure

--- Invoice Management/InvoiceManagement.Infrastructure/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace InvoiceManagement.Infrastructure
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Invoice Management/InvoiceManagement.Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using InvoiceManagement.Infrastructure.Interface;
+using InvoiceManagement.Infrastructure.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace InvoiceManagement.Infrastructure.Repository
+{
+    public abstract class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+            => await _dbSet.ToListAsync();
+
+        public virtual async Task<T> GetByIdAsync(int id)
+            => await _dbSet.FindAsync(id);
+
+        public virtual async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+            => await _dbSet.Where(predicate).ToListAsync();
+
+        public virtual Task AddAsync(T entity)
+        {
+            _dbSet.Add(entity);
+            return Task.CompletedTask;
+        }
+
+        public virtual Task<T> Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            return Task.FromResult(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
