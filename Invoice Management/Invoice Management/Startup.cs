9229607c79f86de1e4a5092ef3331a6dc4e9ed2b
Monde Deyi <mundaright39@example.com>
2026-03-18 04:42:20 +0200

    seeding roles

--- Invoice Management/Invoice Management/Startup.cs
+++ Invoice Management/Invoice Management/Startup.cs
@@ -1,24 +1,27 @@
-﻿using Microsoft.Owin;
+﻿using InvoiceManagement.Infrastructure__new_.Identity;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(Invoice_Management.Startup))]
 namespace Invoice_Management
 {
-        public class Startup
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
         {
-            public void Configuration(IAppBuilder app)
-            {
-                ConfigureAuth(app);
-            }
+            ConfigureAuth(app);
+            // Seed roles on startup
+            RoleInitializer.SeedRoles();
+        }
 
-            public void ConfigureAuth(IAppBuilder app)
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Cookie authentication
+            app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions
             {
-                // Cookie authentication
-                app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions
-                {
-                    AuthenticationType = Microsoft.AspNet.Identity.DefaultAuthenticationTypes.ApplicationCookie,
-                    LoginPath = new PathString("/Account/Login")
-                });
-            }
+                AuthenticationType = Microsoft.AspNet.Identity.DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
         }
     }
+}
---
+++ Invoice Management/InvoiceManagement.Infrastructure (new)/Identity/RoleInitializer.cs
@@ -0,0 +1,30 @@
+﻿using InvoiceManagement.Infrastructure.Persistance;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+namespace InvoiceManagement.Infrastructure__new_.Identity
+{
+    
+    public static class RoleInitializer
+    {
+        public static void SeedRoles()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var roleManager = new RoleManager<IdentityRole>(
+                    new RoleStore<IdentityRole>(context));
+
+                // Check and create roles
+                if (!roleManager.RoleExists(Roles.Admin))
+                    roleManager.Create(new IdentityRole(Roles.Admin));
+
+                if (!roleManager.RoleExists(Roles.Manager))
+                    roleManager.Create(new IdentityRole(Roles.Manager));
+
+                if (!roleManager.RoleExists(Roles.User))
+                    roleManager.Create(new IdentityRole(Roles.User));
+            }
+        }
+    }
+}
