6e82bc691c9268530df30a3371f4ce688ea8b83b
Monde Deyi <mundaright39@example.com>
2026-03-18 02:56:43 +0200

    dbcontext updated

--- Invoice Management/InvoiceManagement.Infrastructure/Persistance/ApplicationDbContext.cs
+++ Invoice Management/InvoiceManagement.Infrastructure/Persistance/ApplicationDbContext.cs
@@ -1,14 +1,20 @@
-﻿using System.Data.Entity;
-using System.Collections.Generic;
+﻿using InvoiceManagement.Infrastructure.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace InvoiceManagement.Infrastructure.Persistance
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
 
         public ApplicationDbContext()
             : base("name=DefaultConnection")
         {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
         }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
