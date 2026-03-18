3227a5fe4d4806300b5586e57134206cb04ea5fa
Monde Deyi <mundaright39@example.com>
2026-03-18 03:52:33 +0200

    startup class added

---
+++ Invoice Management/Invoice Management/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Invoice_Management.Startup))]
+namespace Invoice_Management
+{
+        public class Startup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                ConfigureAuth(app);
+            }
+
+            public void ConfigureAuth(IAppBuilder app)
+            {
+                // Cookie authentication
+                app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions
+                {
+                    AuthenticationType = Microsoft.AspNet.Identity.DefaultAuthenticationTypes.ApplicationCookie,
+                    LoginPath = new PathString("/Account/Login")
+                });
+            }
+        }
+    }
