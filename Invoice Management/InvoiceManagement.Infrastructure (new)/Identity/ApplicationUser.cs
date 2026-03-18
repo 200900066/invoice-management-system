0cd6ff28e7222c8c08402500ce81eaa9f95265a8
Monde Deyi <mundaright39@example.com>
2026-03-18 02:55:52 +0200

    application user model added

---
+++ Invoice Management/InvoiceManagement.Infrastructure (new)/Identity/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+
+namespace InvoiceManagement.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => FirstName + " " + LastName;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsActive { get; set; } = true;
+    }
+}
