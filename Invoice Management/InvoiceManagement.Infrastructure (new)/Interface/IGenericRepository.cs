0516bfd984019c02abcde8800bca72985a72e08e
Monde Deyi <mundaright39@example.com>
2026-03-19 17:38:49 +0200

    added methodes to allow attach navigation links

--- Invoice Management/InvoiceManagement.Infrastructure (new)/Interface/IGenericRepository.cs
+++ Invoice Management/InvoiceManagement.Infrastructure (new)/Interface/IGenericRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
@@ -8,6 +9,10 @@ namespace InvoiceManagement.Infrastructure.Interface
     public interface IGenericRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate = null,Func<IQueryable<T>, IQueryable<T>> include = null);
+
+        Task<T> GetSingleAsync( Expression<Func<T, bool>> predicate,Func<IQueryable<T>, IQueryable<T>> include = null);
+
         Task<T> GetByIdAsync(object id);
         Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
         Task AddAsync(T entity);
