994ec276efee296b545559e0cfc2719b28af61f7
Monde Deyi <mundaright39@example.com>
2026-03-22 14:17:11 +0200

    repots done

---
+++ Invoice Management/Invoice Management/Models/ViewModels/ProductSalesViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Invoice_Management.Models.ViewModels
+{
+    public class ProductSalesViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int QuantitySold { get; set; }
+        public int StockRemaining { get; set; }
+    }
+}
---
+++ Invoice Management/Invoice Management/Models/ViewModels/ReportsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Invoice_Management.Models.ViewModels
+{
+    public class ReportsViewModel
+    {
+        public int TotalProducts { get; set; }
+        public int TotalProductsSold { get; set; }
+        public int TotalStock { get; set; }
+        public int TotalInStock { get; set; }
+        public int TotalSold { get; set; }
+        public List<ProductSalesViewModel> ProductSales { get; set; }
+    }
+}
